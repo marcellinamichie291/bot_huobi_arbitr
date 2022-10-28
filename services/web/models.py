939a15835fb9829c448a66911fe5904f9fa4c6cf
Alex Belocki <abelockii80@mail.ru>
2022-10-28 12:15:23 +0300

    сделал вычисление спреда

--- services/web/models.py
+++ services/web/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from sqlalchemy.orm import reconstructor
+from sqlalchemy.orm import object_session, reconstructor
 from werkzeug.security import check_password_hash
 
 from db import db
@@ -29,7 +29,7 @@ class CurrencyPair(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     pair = db.Column(db.String, nullable=False, unique=True)
-    rate = db.Column(db.Numeric, nullable=False, default=1)
+    rate = db.Column(db.Numeric(asdecimal=False), nullable=False, default=1)
 
     bundle_list = db.relationship(
         'Bundle', 
@@ -58,10 +58,12 @@ class Bundle(db.Model):
 
     def __init__(self, *args, **kwargs) -> None:
         self._pairs_list = None
+        self.start_sum = 15
 
     @reconstructor
     def init_on_load(self, *args, **kwargs):
         self._pairs_list = None
+        self.start_sum = 15
 
 
     def _get_pair(self, pair_id):
@@ -94,11 +96,69 @@ class Bundle(db.Model):
         start_set = set(curr_list[0].split('/'))
         end_set = set(curr_list[-1].split('/'))
         main_currency_set = start_set & end_set
+
+        if not main_currency_set:
+            raise ValueError(f'Невозможно вычислить валюту входа и выхода')
+
         return main_currency_set.pop()
 
+    def get_method(self, from_curr: str, pair: str):
+        if from_curr == pair.split('/')[-1]:
+            return 'division'
+        elif from_curr == pair.split('/')[0]:
+            return 'multiplication'
+        return 'error'
 
-    def from_curr(self):
-        pass
+    def in_usdt(self, currency_name, sum):
+        """
+        Переводит криптовалюту в USDT
+        """
+        if currency_name == 'USDT':
+            return sum
+
+        pair = f'{currency_name}/USDT'
+        rate = (
+            object_session(self).query(CurrencyPair)
+            .filter_by(pair=pair)
+            .first()
+        )
+        if not rate:
+            raise ValueError(f'Не задан курс для пары {pair} (для вычисления эквивалента в USDT)')
+
+        return round(rate.rate * sum, 8)
+
+    def spread(self, usdt=False):
+        """
+        Вычисляем спред в основной валюте или в USDT
+        """
+        if usdt:
+            start_sum = self.in_usdt(self.main_currency, self.start_sum)
+            end_sum = self.in_usdt(self.main_currency, self.calc_end_result())
+            return round(end_sum / start_sum * 100 - 100, 2)
+
+        return round(self.calc_end_result() / self.start_sum * 100 - 100, 2)
+        
+    def calc_end_result(self):
+        """
+        Вычисляем конечный результат после прохода 
+        всех пар в связке. Результат в основной валюте
+        """
+        from_curr = self.main_currency
+
+        result = self.start_sum
+        for pair in self.pairs_list:
+            method = self.get_method(from_curr, pair.pair)
+            if method == 'division':
+                result /= pair.rate
+                from_curr = pair.pair.split('/')[0]
+
+            elif method == 'multiplication':
+                result *= pair.rate
+                from_curr = pair.pair.split('/')[-1]
+
+        return result
+
+
 
 
 
